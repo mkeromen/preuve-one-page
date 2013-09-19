14dc6d9fc2c9e545ca624ae4dcd9d676ff1a1688
Denis Laliberté <denis.laliberte@gmail.com>
2013-09-19 18:00:16 -0400

    génération du controller page et première route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 PreuveOnePage::Application.routes.draw do
+  get "page/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root to: 'page#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
