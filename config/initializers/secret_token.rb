b8385a581785ffe528bdc53b32d070fc6316b0ea
Denis Laliberté <denis.laliberte@gmail.com>
2013-09-19 16:49:51 -0400

    commit initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PreuveOnePage::Application.config.secret_key_base = '5b00943101c30b7b526ad33db0f0702accbd2fd90382a5ff38eb7d26236802efd94b8e55d80add519cf783bdb5840de1db6fa8b7c97be75a16992ee420e33c64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PreuveOnePage::Application.config.session_store :cookie_store, key: '_preuve-one-page_session'
