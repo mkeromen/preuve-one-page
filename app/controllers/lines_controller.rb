5b3aa012f6c91871ddaca2fd5fd05becc3391409
Denis Laliberté <denis.laliberte@gmail.com>
2013-09-19 18:57:20 -0400

    création des méthode create show index de la line

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -2,7 +2,21 @@ class LinesController < ApplicationController
   def new
   end
   def create
-    render text: params[:line].inspect
+    @line = Line.new(line_params)
+
+    @line.save
+    redirect_to @line
   end
 
+  def show
+    @line = Line.find(params[:id])
+  end
+  def index
+    @line = Line.all
+
+  end
+  private
+    def line_params
+      params.require(:line).permit(:title,:text)
+    end
 end
