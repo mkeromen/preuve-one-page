7db7979c9516ca75c47a936c915dc7767f2b884d
mkeromen <mattbzh@gmail.com>
2013-10-16 16:12:09 -0400

    Edit in place sous la liste des lignes

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -12,13 +12,14 @@ class LinesController < ApplicationController
 
   def index
     @lines = Line.all
+	@line = Line.new({:title => "Ajouter un titre", :text => "Ajouter un texte"})
   end
 
   respond_to :html, :json
   def update
     @line = Line.find(params[:id])
-    @line.update_attributes(line_params)
-    respond_with @line
+    @line.update_attributes(line_params)  
+	respond_with @line
   end
 
   private
